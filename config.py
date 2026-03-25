d48b3e28fb9c9f1ac0839c737744715fda9b9949
jetrolet <xpiatu@gmail.com>
2026-03-26 00:28:04 +0700

    Refactor config to use environment variables

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-BOT_TOKEN = "8703742903:AAFq--XFq2wFFGAtmB2ApwQcpOYKg1ps-cI"
-ADMIN_ID = 35055316  # ganti dengan ID telegram kamu
-PAYMENT_TIMEOUT = 1200  # 20 menit
-QRIS_IMAGE = "qris.jpg"  # file QRIS Dana Bisnis
+import os
+
+# Token bot Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8703742903:AAFq--XFq2wFFGAtmB2ApwQcpOYKg1ps-cI")
+
+# ID admin Telegram
+ADMIN_ID = int(os.getenv("ADMIN_ID", 35055316))
+
+# Timeout pembayaran dalam detik (misal 20 menit = 1200 detik)
+PAYMENT_TIMEOUT = int(os.getenv("PAYMENT_TIMEOUT", 1200))
+
+# Nama file QRIS untuk pembayaran
+QRIS_IMAGE = "qris.jpg"
