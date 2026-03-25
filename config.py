f163c2fffbd8fb7b922c922251b64f23148c2208
jetrolet <xpiatu@gmail.com>
2026-03-26 00:15:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8703742903:AAHX017RH2vnqie5xKBVKymR2R2YXWzN-SI"
+BOT_TOKEN = "8703742903:AAFq--XFq2wFFGAtmB2ApwQcpOYKg1ps-cI"
 ADMIN_ID = 35055316  # ganti dengan ID telegram kamu
 PAYMENT_TIMEOUT = 1200  # 20 menit
 QRIS_IMAGE = "qris.jpg"  # file QRIS Dana Bisnis
